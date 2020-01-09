5dc6df29b5804300f0d48cfb421df936e6f2133d
mayokunthefirst <adeniyimayokun17@gmail.com>
2020-01-09 01:29:24 +0100

    Add dog breed model

---
+++ app/src/main/java/com/example/aboutdogs/model/Model.kt
@@ -0,0 +1,15 @@
+package com.example.aboutdogs.model
+
+/**
+ * Created by Mayokun Adeniyi on 08/01/2020.
+ */
+
+data class DogBreed(
+    val breedId: String?,
+    val dogBreed: String?,
+    val lifeSpan: String?,
+    val breedGroup: String?,
+    val bredFor: String?,
+    val temperament: String?,
+    val imageUrl: String?
+)
