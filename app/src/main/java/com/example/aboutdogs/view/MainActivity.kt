8f4d3cd0313110854704fc6d31bf821ef4d64a4f
mayokunthefirst <adeniyimayokun17@gmail.com>
2020-01-09 16:32:21 +0100

    Feat: add up arrow on action bar

--- app/src/main/java/com/example/aboutdogs/view/MainActivity.kt
+++ app/src/main/java/com/example/aboutdogs/view/MainActivity.kt
@@ -2,13 +2,23 @@ package com.example.aboutdogs.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.ui.NavigationUI
 import com.example.aboutdogs.R
 import timber.log.Timber
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        navController = Navigation.findNavController(this,R.id.fragment)
+        NavigationUI.setupActionBarWithNavController(this,navController)
     }
+
+    override fun onSupportNavigateUp() = NavigationUI.navigateUp(navController,null)
 }
