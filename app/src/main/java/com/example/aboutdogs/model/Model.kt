e2ea8fbbcc92e216fa0a08a21e0273aefd91a145
mayokunthefirst <adeniyimayokun17@gmail.com>
2020-01-12 18:19:12 +0100

    Add baseviewmodel

--- app/src/main/java/com/example/aboutdogs/model/Model.kt
+++ app/src/main/java/com/example/aboutdogs/model/Model.kt
@@ -1,30 +1,43 @@
 package com.example.aboutdogs.model
 
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
 import com.google.gson.annotations.SerializedName
 
 /**
  * Created by Mayokun Adeniyi on 08/01/2020.
  */
 
+@Entity
 data class DogBreed(
-    @SerializedName("id")
-    val breedId: String?,
+        @ColumnInfo(name = "breed_id")
+        @SerializedName("id")
+        val breedId: String?,
 
-    @SerializedName("name")
-    val dogBreed: String?,
+        @ColumnInfo(name = "dog_name")
+        @SerializedName("name")
+        val dogBreed: String?,
 
-    @SerializedName("life_span")
-    val lifeSpan: String?,
+        @ColumnInfo(name = "life_span")
+        @SerializedName("life_span")
+        val lifeSpan: String?,
 
-    @SerializedName("breed_group")
-    val breedGroup: String?,
+        @ColumnInfo(name = "breed_group")
+        @SerializedName("breed_group")
+        val breedGroup: String?,
 
-    @SerializedName("bred_for")
-    val bredFor: String?,
+        @ColumnInfo(name = "bred_for")
+        @SerializedName("bred_for")
+        val bredFor: String?,
 
-    @SerializedName("temperament")
-    val temperament: String?,
+        @SerializedName("temperament")
+        val temperament: String?,
 
-    @SerializedName("url")
-    val imageUrl: String?
-)
+        @ColumnInfo(name = "dog_url")
+        @SerializedName("url")
+        val imageUrl: String?
+) {
+    @PrimaryKey(autoGenerate = true)
+    var uuid: Int = 0
+}
--- app/src/main/java/com/example/aboutdogs/viewmodel/ListViewModel.kt
+++ app/src/main/java/com/example/aboutdogs/viewmodel/ListViewModel.kt
@@ -1,23 +1,27 @@
 package com.example.aboutdogs.viewmodel
 
+import android.app.Application
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.example.aboutdogs.database.DogDao
+import com.example.aboutdogs.database.DogDatabase
 import com.example.aboutdogs.model.DogBreed
 import com.example.aboutdogs.network.DogApiService
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.CompositeDisposable
 import io.reactivex.observers.DisposableSingleObserver
 import io.reactivex.schedulers.Schedulers
+import kotlinx.coroutines.launch
 
 /**
  * Created by Mayokun Adeniyi on 08/01/2020.
  */
-class ListViewModel: ViewModel() {
+class ListViewModel(application: Application): BaseViewModel(application) {
 
     private val disposable = CompositeDisposable()
     private val dogService = DogApiService()
-
+    private val dao = DogDatabase.getInstance(getApplication()).dogDao
 
     private val _dogs = MutableLiveData<List<DogBreed>>()
     val dogs: LiveData<List<DogBreed>>
@@ -43,9 +47,7 @@ class ListViewModel: ViewModel() {
                 .observeOn(AndroidSchedulers.mainThread())
                 .subscribeWith(object: DisposableSingleObserver<List<DogBreed>>(){
                     override fun onSuccess(dogList: List<DogBreed>) {
-                        _dogs.value = dogList
-                        _dogError.value = false
-                        _loading.value = false
+                      storeDogsLocally(dogList)
                     }
 
                     override fun onError(e: Throwable) {
@@ -56,6 +58,24 @@ class ListViewModel: ViewModel() {
 
                 })
         )
+    }
+
+    private fun dogsRetrieved(dogList: List<DogBreed>){
+        _dogs.value = dogList
+        _dogError.value = false
+        _loading.value = false
+    }
+    private fun storeDogsLocally(dogList: List<DogBreed>) {
+        launch {
+            dao.deleteAllDogs()
+            val result = dao.insertAll(*dogList.toTypedArray())
+            var i = 0
+            while (i < dogList.size){
+                dogList[i].uuid = result[i].toInt()
+                ++i
+            }
+            dogsRetrieved(dogList)
+        }
     }
 
     override fun onCleared() {
