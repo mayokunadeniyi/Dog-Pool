958fef78e96a95b3a5a49007ee85651bfd4d13a1
mayokunthefirst <adeniyimayokun17@gmail.com>
2020-01-14 04:21:09 +0100

    Use data binding

--- app/src/main/java/com/example/aboutdogs/utils/Util.kt
+++ app/src/main/java/com/example/aboutdogs/utils/Util.kt
@@ -2,6 +2,7 @@ package com.example.aboutdogs.utils
 
 import android.content.Context
 import android.widget.ImageView
+import androidx.databinding.BindingAdapter
 import androidx.swiperefreshlayout.widget.CircularProgressDrawable
 import com.bumptech.glide.Glide
 import com.bumptech.glide.request.RequestOptions
@@ -27,4 +28,9 @@ fun ImageView.loadImage(uri: String?, progressDrawable: CircularProgressDrawable
         .setDefaultRequestOptions(options)
         .load(uri)
         .into(this)
+}
+
+@BindingAdapter("imageUrl")
+fun ImageView.setImageUrl(uri: String?){
+   this.loadImage(uri, getProgressDrawable(context))
 }
